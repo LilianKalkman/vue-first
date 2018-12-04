02fe145faf23ab6ad67494274182838de2c58095
LilianKalkman <liliankalkman@hotmail.com>
2018-12-04 14:54:32 +0100

    first commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ quote }}</h1>
+    <p>This is the Test Component created</p>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String
+  },
+  data() {
+    return {
+      quote: "hello"
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  color: pink;
+}
+h1 {
+  color: blue;
+}
+</style>
---
+++ src/views/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="test">
+    <h1>This is an test page</h1>
+    <p>{{ value }}</p>
+    <p>{{ name }}</p>
+    <Test message="Test Prop doorgegeven"/>
+  </div>
+</template>
+
+<script>
+import Test from "@/components/EventCard.vue";
+
+export default {
+  name: "test",
+  components: {
+    Test
+  },
+  data: function() {
+    return {
+      value: 3,
+      name: "Lilian"
+    };
+  }
+};
+</script>
